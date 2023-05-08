da43b29b11ca0755a69b46bc71be9c2a91c9ede8
wladarius <logvladarius@gmail.com.com>
2023-05-08 14:59:37 +0300

    srtu

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,11 @@
 const { src, dest, series, watch } = require('gulp')
+const concat = require('gulp-concat')
+
+
+const styles = () => {
+    return src('src/styles/**/*.css')
+        .pipe(concat('main.css'))
+        .pipe(dest('dist'))
+}
+
+exports.styles = styles
